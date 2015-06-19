96c89489f68c3d86f9caa28eca4feded55eb5597
atillabyte <atillabyte@null.net>
2015-06-19 11:57:45 -0400

    A bit more tidying.

--- ipify/src/ipify/ipify.cs
+++ ipify/src/ipify/ipify.cs
@@ -1,46 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 
 public static class ipify
 {
-    public static class Settings
+    public class Settings
     {
-        public static bool ALLOW_USER_AGENT = true;
-        public static string API_URL = "https://api.ipify.org";
-        public static uint MAX_TRIES = 3;
-        public static IWebProxy PROXY = null;
-        public static int TRIES_DURATION = 0x3e8;
-        public static string USER_AGENT = string.Format("csharp-ipify/{0} csharp/{1} {2}", VERSION, Environment.Version, Environment.OSVersion);
-        public static bool USING_PROXY = false;
+        public bool ALLOW_USER_AGENT = true;
+        public string API_URL = "https://api.ipify.org";
+        public uint MAX_TRIES = 3;
+        public IWebProxy PROXY = null;
+        public int TRIES_DURATION = 0x3e8;
+        public string USER_AGENT = string.Format("csharp-ipify/{0} csharp/{1} {2}", VERSION, Environment.Version, Environment.OSVersion);
+        public bool USING_PROXY = false;
         private static string VERSION = "1.0.0";
     }
-
+    
     public enum Format
     {
         Raw, JSON, JSONP
     }
+
+    public static Settings settings = new Settings();
 
     public static string Resolve(Format format = Format.Raw, string additional_args = null)
     {
         string result = null;
         object last_response = null;
 
-        using (OWebClient webClient = new OWebClient() { Proxy = (Settings.USING_PROXY) ? Settings.PROXY : null, Headers = new WebHeaderCollection() })
+        using (OWebClient webClient = new OWebClient() { Proxy = (settings.USING_PROXY) ? settings.PROXY : null, Headers = new WebHeaderCollection() })
         {
-            if (Settings.ALLOW_USER_AGENT)
-                webClient.Headers.Add("User-Agent", Settings.USER_AGENT);
+            if (settings.ALLOW_USER_AGENT)
+                webClient.Headers.Add("User-Agent", settings.USER_AGENT);
 
-            for (int tries = 0; tries < Settings.MAX_TRIES; tries++)
+            for (int tries = 0; tries < settings.MAX_TRIES; tries++)
             {
                 try {
-                    result = webClient.DownloadString(string.Format("{0}/?format={1}&{2}", Settings.API_URL, format.ToString(), additional_args));
+                    result = webClient.DownloadString(string.Format("{0}/?format={1}&{2}", settings.API_URL, format.ToString(), additional_args));
                 }
                 catch (WebException e) {
                     last_response = e;
-                    Task.Delay(Settings.TRIES_DURATION);
+                    System.Threading.Thread.Sleep(settings.TRIES_DURATION);
                 }
             }
         }
